c4eead0c4cf69560b11a874d0e868f2d9c9291e5
lina-bh <36717206+lina-bh@users.noreply.github.com>
2025-09-19 12:55:53 +0100

    cnpg barman setup

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,11 @@ output "k3s_token" {
 output "server_host" {
   value = try(module.instance[0].tailscale_ip, "")
 }
+
+output "oci_os_ns" {
+  value = data.oci_objectstorage_namespace.ns.namespace
+}
+
+output "oci_os_s3_endpoint" {
+  value = "https://${data.oci_objectstorage_namespace.ns.namespace}.compat.objectstorage.uk-london-1.oraclecloud.com"
+}
