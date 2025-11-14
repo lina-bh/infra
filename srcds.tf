073323bbd55dd6cbcf06d5f8563c2c57902a1bee
lina-bh <36717206+lina-bh@users.noreply.github.com>
2025-11-14 16:21:43 +0000

    fmt

--- srcds.tf
+++ srcds.tf
@@ -1,10 +1,10 @@
 data "oci_core_images" "ubuntu" {
-  compartment_id = oci_core_subnet.sn.compartment_id
-  shape = "VM.Standard.E2.1.Micro"
-  operating_system = "Canonical Ubuntu"
+  compartment_id           = oci_core_subnet.sn.compartment_id
+  shape                    = "VM.Standard.E2.1.Micro"
+  operating_system         = "Canonical Ubuntu"
   operating_system_version = "24.04"
-  sort_by = "TIMECREATED"
-  sort_order = "DESC"
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
 }
 
 resource "oci_core_vcn" "pub" {
@@ -15,7 +15,7 @@ resource "oci_core_vcn" "pub" {
 
 resource "oci_core_internet_gateway" "inet" {
   compartment_id = oci_core_vcn.pub.compartment_id
-  vcn_id = oci_core_vcn.pub.id
+  vcn_id         = oci_core_vcn.pub.id
 }
 
 resource "oci_core_default_route_table" "pub" {
@@ -37,10 +37,10 @@ resource "oci_core_subnet" "pub" {
 }
 
 resource "oci_core_default_security_list" "pub" {
-  compartment_id = oci_core_vcn.pub.compartment_id
+  compartment_id             = oci_core_vcn.pub.compartment_id
   manage_default_resource_id = oci_core_vcn.pub.default_security_list_id
   ingress_security_rules {
-    source = "5.151.0.0/16"
+    source   = "5.151.0.0/16"
     protocol = local.security_list_protocol.TCP
     tcp_options {
       min = 22
@@ -49,7 +49,7 @@ resource "oci_core_default_security_list" "pub" {
   }
   egress_security_rules {
     destination = "0.0.0.0/0"
-    protocol = local.security_list_protocol.TCP
+    protocol    = local.security_list_protocol.TCP
     tcp_options {
       min = 1
       max = 25565
@@ -57,7 +57,7 @@ resource "oci_core_default_security_list" "pub" {
   }
   egress_security_rules {
     destination = "0.0.0.0/0"
-    protocol = local.security_list_protocol.UDP
+    protocol    = local.security_list_protocol.UDP
     udp_options {
       min = 1
       max = 25565
@@ -74,17 +74,20 @@ data "cloudinit_config" "srcds" {
 }
 
 resource "oci_core_instance" "srcds" {
-  compartment_id = oci_core_subnet.pub.compartment_id
+  lifecycle {
+    ignore_changes = [source_details]
+  }
+  compartment_id      = oci_core_subnet.pub.compartment_id
   availability_domain = data.oci_identity_availability_domains.ad.availability_domains[2].name
-  shape = "VM.Standard.E2.1.Micro"
+  shape               = "VM.Standard.E2.1.Micro"
   source_details {
-    source_type = "image"
-    source_id = data.oci_core_images.ubuntu.images[0].id
+    source_type             = "image"
+    source_id               = data.oci_core_images.ubuntu.images[0].id
     boot_volume_size_in_gbs = 50
     boot_volume_vpus_per_gb = 20
   }
   create_vnic_details {
-    subnet_id = oci_core_subnet.pub.id
+    subnet_id        = oci_core_subnet.pub.id
     assign_public_ip = true
   }
   agent_config {
