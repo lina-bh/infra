f7ccea1e4eb39369d25f593fcd1017ebf4e99d35
lina-bh <36717206+lina-bh@users.noreply.github.com>
2025-12-01 22:04:56 +0000

    make tailscale acl more finely grained
    simplify mullvad attribute
    replace rdp with sunshine

--- tailscale_acl.tf
+++ tailscale_acl.tf
@@ -1,27 +1,61 @@
 data "tailscale_devices" "devs" {
 }
 
+data "tailscale_device" "framework" {
+  name = "framework.${var.tailnet}"
+}
+
+data "tailscale_device" "iphone-15-pro" {
+  name = "iphone-15-pro.${var.tailnet}"
+}
+
+data "tailscale_device" "kobo" {
+  name = "kobo.${var.tailnet}"
+}
+
+locals {
+  kde-connect-devices = flatten([for device in [data.tailscale_device.framework, data.tailscale_device.iphone-15-pro] : device.addresses])
+  mullvad-devices     = local.kde-connect-devices
+}
+
 # https://tailscale.com/kb/1337/acl-syntax
 resource "tailscale_acl" "acl" {
   acl = jsonencode({
     grants = [
+      # connect to one's own devices with SSH
       {
         src = ["autogroup:member"]
         dst = ["autogroup:self"]
-        ip  = ["tcp:22", "1714-1764", "tcp:2222", "3389"]
+        ip  = ["tcp:22"]
       },
+      # tailnet owner can connect to tagged devices with SSH
       {
         src = ["autogroup:owner"]
         dst = ["autogroup:tagged"]
         ip  = ["tcp:22"]
       },
+      # my devices can use kde connect and sunshine
+      {
+        src = local.kde-connect-devices
+        dst = local.kde-connect-devices
+        ip  = ["1714-1746", "tcp:47984-48010", "udp:47998-48000"]
+      },
+      # unprivileged ssh port for kobo
+      {
+        src = ["lina-bh@github"]
+        dst = data.tailscale_device.kobo.addresses
+        ip  = ["tcp:2222"]
+      },
+      # oci instances can use vxlan on 8472 udp and apiserver on 6443
       {
         src = ["tag:oci"]
         dst = ["tag:oci"]
         ip  = ["tcp:6443", "udp:8472"]
       },
+      # i can connect to apiserver proxy with cluster admin role, and all k8s ingresses
+      # some ingresses do not have authentication (longhorn dashboard)
       {
-        src = ["autogroup:owner"]
+        src = ["lina-bh@github"]
         dst = ["tag:k8s-operator"]
         ip  = ["tcp:443"]
         app = {
@@ -35,18 +69,20 @@ resource "tailscale_acl" "acl" {
         }
       },
       {
-        src = ["autogroup:owner"]
+        src = ["lina-bh@github"]
         dst = ["tag:k8s"]
         ip  = ["tcp:443"]
       }
     ]
     ssh = [
+      # any user can SSH into their own device as any user
       {
         action = "accept"
         src    = ["autogroup:member"]
         dst    = ["autogroup:self"]
         users  = ["root", "autogroup:nonroot"]
       },
+      # tailnet owner can SSH into any tagged device
       {
         action = "accept"
         src    = ["autogroup:owner"]
@@ -55,13 +91,10 @@ resource "tailscale_acl" "acl" {
       },
     ]
     nodeAttrs = [
+      # allow mullvad to static list of devices
       {
-        target = flatten(
-          [
-            for device in data.tailscale_devices.devs.devices : device.addresses if contains(["framework", "frameworkquadlet", "iphone-15-pro"], trimsuffix(device.name, nonsensitive(".${var.tailnet}")))
-          ]
-        )
-        attr = ["mullvad"]
+        target = local.mullvad-devices
+        attr   = ["mullvad"]
       },
     ]
     tagOwners = {
